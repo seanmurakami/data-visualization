00ce9fe5d457ab3baf2c78b1133e3e31b2b9411b
Sean Murakami <murakami.sean@yahoo.com>
2020-05-20 09:17:21 -0700

    created sifting out visualization for graph

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import produce from "immer";
 
 const rowCount = 30;
 const colCount = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+];
 
 function App() {
   const [grid, setGrid] = useState(() => {
@@ -13,34 +20,68 @@ function App() {
     return rows;
   });
 
+  const selectCell = (x, y) => {
+    const newGrid = produce(grid, (gridCopy) => {
+      gridCopy[x][y] = 1;
+    });
+    setGrid(newGrid);
+  };
+
+  const startLookup = useCallback(() => {
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < rowCount; i++) {
+          for (let j = 0; j < colCount; j++) {
+            operations.forEach(([x, y]) => {
+              if (g[i][j]) {
+                const newX = i + x;
+                const newY = j + y;
+                if (
+                  newX >= 0 &&
+                  newX < rowCount &&
+                  newY >= 0 &&
+                  newY < colCount
+                ) {
+                  gridCopy[newX][newY] = 1;
+                }
+              }
+            });
+          }
+        }
+      });
+    });
+
+    setTimeout(startLookup, 100);
+  }, []);
+
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${colCount}, 26px)`,
-        gridTemplateRows: `repeat(${rowCount}, 26px)`,
-      }}
-    >
-      {grid.map((rows, i) =>
-        rows.map((col, k) => (
-          <div
-            key={`${i} - ${k}`}
-            style={{
-              width: 25,
-              height: 25,
-              backgroundColor: grid[i][k] ? "pink" : undefined,
-              border: "1px solid black",
-            }}
-            onClick={() => {
-              const newGrid = produce(grid, (gridCopy) => {
-                gridCopy[i][k] = 1;
-              });
-              setGrid(newGrid);
-            }}
-          />
-        ))
-      )}
-    </div>
+    <>
+      <button onClick={startLookup}>start</button>
+      <div
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          marginTop: "60px",
+          gridTemplateColumns: `repeat(${colCount}, 26px)`,
+          gridTemplateRows: `repeat(${rowCount}, 26px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i} - ${k}`}
+              style={{
+                width: 25,
+                height: 25,
+                backgroundColor: grid[i][k] ? "blue" : undefined,
+                border: "1px solid black",
+              }}
+              onClick={() => selectCell(i, k)}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
